28d20d4f406933050f304f3b7fab8cf0a915a67b
Jeisson Alonso Bermúdez Nieto <102543179+Jeisson05@users.noreply.github.com>
2022-04-11 22:28:52 -0500

    Delete Talleres Python directory

--- Talleres Python/ejercicio_7.py
+++
@@ -1,8 +0,0 @@
-#Entradas
-metros=float(input("Digite una cantidad en metros: "))
-#Caja negra
-pies=metros*3.281
-pulgadas=metros*39.37
-#Salidas
-print("Los metros convertidos a pies equivalen a: ",round(pies,2))
-print("Los metros convertidos a pulgadas equivalen a: ",round(pulgadas,2))
