1d67a837abb9e7f9451a269741f72ceeec11aba8
Jeisson Alonso Bermúdez Nieto <102543179+Jeisson05@users.noreply.github.com>
2022-05-15 22:16:40 -0500

    Add files via upload

---
+++ Taller Estructuras de Control For.py
@@ -0,0 +1,182 @@
+archivo = open('paises.txt', 'r')
+#imprima la posicion de colombia
+"""
+c=0
+lista=[]
+for i in archivo:
+  lista.append(i)
+  a=" ".join(lista)
+  c=c+1 
+  if(a=="Colombia: Bogotá\n"):
+    break
+  lista=[]   
+print(c)
+"""
+#Imprima todos los paises
+"""
+lista=[]
+for i in archivo:
+  a=i.index(":")
+  for r in range(0,a):
+    lista.append(i[r])
+  a="".join(lista)
+  print(a)
+  lista=[]
+"""
+#Imprima todas las Capitales
+"""
+lista=[]
+for i in archivo:
+  a=i.index(":")
+  for r in range(a+2,len(i)):
+    lista.append(i[r])
+  a="".join(lista)
+  print(a)
+  lista=[]
+"""  
+#Imprimir todos los paises que inicien con la letra C
+"""
+lista=[]
+paises=[]
+for i in archivo:
+  a=i.index(":")
+  for r in range(0,a):
+    lista.append(i[r])
+  a="".join(lista)
+  paises.append(a)
+  lista=[]
+for i in paises:
+  if(i[0]=="C"):
+    print(i)
+"""  
+#imprima todas las capitales que inicien con la leta B
+"""
+lista=[]
+ciudad=[]
+for i in archivo:
+  a=i.index(":")
+  for r in range(a+2,len(i)-1):
+    lista.append(i[r])
+  a="".join(lista)
+  ciudad.append(a)
+  lista=[]
+for i in ciudad:
+  if(i[0]=="B"):
+    print(i)  
+"""
+#Cuente e imprima cuantas ciudades inician con la latra M  
+"""
+lista=[]
+ciudades=[]
+for i in archivo:
+  a=i.index(":")
+  for r in range(a+2,len(i)-1):
+    lista.append(i[r])
+  a="".join(lista)
+  ciudades.append(a)
+  lista=[]
+c=0
+for i in ciudades:
+  if(i[0]=="M"):
+    print(i)
+    c=c+1
+print(c)
+"""
+#Imprima todos los paises y capitales, cuyo inicio sea con la letra U
+"""
+lista=[]
+paises=[]
+for i in archivo:
+  a=i.index(":")
+  for r in range(0,a):
+    lista.append(i[r])
+  a="".join(lista)
+  paises.append(a)
+  lista=[]
+for i in paises:
+  if(i[0]=="U"):
+    print(i)
+"""
+#Cuente e imprima cuantos paises que hay en el archivo
+"""
+c=0
+lista=[]
+for i in archivo:
+  a=i.index(":")
+  for r in range(0,a):
+    lista.append(i[r])
+  a="".join(lista)
+  print(a)
+  lista=[]
+  c=c+1
+print(c)
+"""
+#Busque e imprima la ciudad de Singapur
+"""
+c=0
+lista=[]
+paises=[]
+for i in archivo:
+  a=i.index(":")
+  for r in range(0,a):
+    lista.append(i[r])
+  a="".join(lista)
+  print(a)
+  lista=[]
+  if(a=="Singapur: Singapur \n"):
+    c=c+1
+print(c)
+"""
+#Busque e imprima el pais de Venezuela y su capital
+
+#Cuente e imprima las ciudades que su pais inicie con la letra E
+"""
+lista=[]
+ciudades=[]
+for i in archivo:
+  a=i.index(":")
+  for r in range(a+2,len(i)-1):
+    lista.append(i[r])
+  a="".join(lista)
+  ciudades.append(a)
+  lista=[]
+c=0
+for i in ciudades:
+  if(i[0]=="E"):
+    print(i)
+    c=c+1
+print(c)
+"""
+#Buesque e imprima la Capiltal de Colombia
+
+#imprima la posicion del pais de Uganda
+"""
+c=0
+lista=[]
+for i in archivo:
+  lista.append(i)
+  a="".join(lista)
+  c=c+1 
+  if(a=="Uganda: Kampala\n"):
+    break
+  lista=[]   
+print(c)
+"""
+#imprima la posicion del pais de Mexico
+"""
+c=0
+lista=[]
+for i in archivo:
+  lista.append(i)
+  b="".join(lista)
+  c=c+1
+  if(b=="México: Ciudad de México \n"):
+    break
+  lista=[]   
+print(c)
+"""
+"""
+El alcalde de Antananarivo contrato a algunos alumnos de la Universidad Ean para corregir el archivo de países.txt, ya que la capital de Madagascar NO es rey julien es Antananarivo, espero que el alcalde se vaya contento por su trabajo. Utilice un For para cambiar ese Dato
+"""
+#Agregue un país que no esté en la lista 
+archivo.close()
